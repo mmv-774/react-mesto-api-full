80bd87995b11d3a252357b605ce95770d80ca649
MIkhail <mmv-774@yandex.ru>
2022-05-05 19:28:50 +0500

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,19 +4,13 @@ const allowedCors = [
   'localhost:3000',
 ];
 
-module.exports.simpleCors = (req, res, next) => {
+module.exports.cors = (req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.end();
-    return;
   }
 
-  next();
-};
-
-module.exports.difficultCors = (req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
