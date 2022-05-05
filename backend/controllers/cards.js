bc5c0ad721c35fb3c9113c80d512f8c3e8e7b187
MIkhail <mmv-774@yandex.ru>
2022-05-05 22:46:00 +0500

    убрать сортировку карточек по дате

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -21,13 +21,7 @@ const cardQueryErrorHandler = (error, next, messages = {}) => {
 module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => {
-      res.send({
-        cards: cards.sort((a, b) => {
-          if (a.createdAt > b.createdAt) return -1;
-          if (a.createdAt < b.createdAt) return 1;
-          return 0;
-        }),
-      });
+      res.send({ cards });
     })
     .catch((error) => cardQueryErrorHandler(error, next));
 };
