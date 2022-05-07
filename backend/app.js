901b5401ba9eb6c908e6e6c6775bec228b3e6d83
MIkhail <mmv-774@yandex.ru>
2022-05-07 10:54:27 +0500

    краш-тест сервера

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(cors);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', celebrate(signInSchema), login);
 app.post('/signup', celebrate(signUpSchema), createUser);
 app.use('/users', auth, userRouter);
